a7dc9daf46c2a35a94293ac8a3c6153090a0fe87
Małgorzata Brożek <malgorzata.brozek@softwarehut.com>
2020-05-22 18:43:10 +0200

    Add: localStorage functionality

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,11 +1,3 @@
-// const newCard=[{
-//     id: 666,
-//     name: "Gosia",
-//     species: "human", 
-//     image:"https://static.goldenline.pl/user_photo/152/user_1255064_af4a5f_huge.jpg",
-//     status: "Alive", 
-//     gender: "female", 
-//     location: "Earth"}];
 
     const LsName= document.getElementById("LsName");
     const LsSpecies= document.getElementById("LsSpecies");
@@ -20,46 +12,33 @@
     
     buttonSave.onclick= function(){
         LsName.value!="" ? localStorage.setItem("name", LsName.value ) : null;
-        localStorage.setItem("species", LsSpecies.value );
-        localStorage.setItem("status", LsStatus.value ); 
-        localStorage.setItem("image", LsImage.value );  
-        localStorage.setItem("gender", LsGender.value ); 
-        localStorage.setItem("GenLocation", LsLocation.value ); 
+        LsSpecies!="" ? localStorage.setItem("species", LsSpecies.value ) : null;
+        LsStatus!="" ? localStorage.setItem("status", LsStatus.value ) : null; 
+        LsImage!="" ? localStorage.setItem("image", LsImage.value ) : null;  
+        LsGender!="" ? localStorage.setItem("gender", LsGender.value ) : null; 
+        LsLocation!="" ? localStorage.setItem("genLocation", LsLocation.value ) : null; 
         console.log(localStorage);
     }
     buttonCreate.onclick= function(){
-        document.getElementById("new-card").classList.remove('hide');
+        // document.getElementById("new-card").classList.remove('hide');
         name= localStorage.getItem("name");
         species= localStorage.getItem("species");
         status= localStorage.getItem("status");
         gender= localStorage.getItem("gender");
-        GenLocation= localStorage.getItem("GenLocation");
+        genLocation= localStorage.getItem("genLocation");
         image= localStorage.getItem("image");
-        document.getElementById("new-card").innerHTML +=  `
-           <header class="card__header">
-               <h1 class="card__heading"> ${name} </h1>
-               <p class="class__species"> ${species} </p>
-           </header>
-           <img class="card__image" src="${image}" alt="${name}"/>
-           <p class="card__description"> <b> Status:</b> ${status} </p>
-           <p class="card__description"> <b> Gender:</b> ${gender} </p>
-           <p class="card__description"> <b> Location:</b> ${GenLocation} </p>
-        `;
-
-        // for (let i=0; i<localStorage.length; i++){
-        //     const key = localStorage.key(i);
-        //     const value= localStorage.getItem(key);
-    
-        //     document.getElementById("new-card").innerHTML +=  ${key} : ${value}
-            
-        // // }<div class="card">
-        //    <header class="card__header">
-        //        <h1 class="card__heading"> ${name} </h1>
-        //        <p class="class_species"> ${species} </p>
-        //    </header>
-        //    <img class="card__image" src="${image}" alt="${name}"/>
-        //    <p class="card__description"> <b> Status:</b> ${status} </p>
-        //    <p class="card__description"> <b> Gender:</b> ${gender} </p>
-        //    <p class="card__description"> <b> Location:</b> ${location} </p>
-        // </div>
+        document.getElementById("collection-content").insertAdjacentHTML('afterbegin',  `
+            <article class="card">
+                <header class="card__header">
+                    <h1 class="card__heading"> ${name} </h1>
+                    <p class="class__species"> ${species} </p>
+                </header>
+                <img class="card__image" src="${image}" alt="${name}"/>
+                <p class="card__description"> <b> Status:</b> ${status} </p>
+                <p class="card__description"> <b> Gender:</b> ${gender} </p>
+                <p class="card__description"> <b> Location:</b> ${genLocation} </p>
+            </article>`);
     }
+
+
+            
